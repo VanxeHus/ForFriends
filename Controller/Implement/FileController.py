666befb2a5b77be68353945f9bf9185e27f9a94d
VanxeHus <13751378996@163.com>
2018-03-22 14:51:40 +0800

    complete controller

---
+++ Controller/Implement/FileController.py
@@ -0,0 +1,7 @@
+#!usr/bin/python
+#coding=utf-8
+class FileController:
+    def __init__(self):
+        pass
+    def Handle(self):
+        pass
--- Controller/MainController.py
+++ Controller/MainController.py
@@ -1,35 +1,44 @@
 #!usr/bin/python
-#coding=utf-8
+# coding=utf-8
 from Implement import LoginController as Login
 from Implement import NotfindController as Notfind
+from Implement import FileController as File
+
+
 class MainController:
     def __init__(self):
-        self.cMap={}
-    #分发数据包到各个子controller
-    #data包括header和body
-    def Handle(self,sck,addr,data):
-        if len(data.header)<=0 or len(data.body)<=0:
-            raise NameError,"data.header or data.body is null"
-        if self.cMap.has_key(data.header[1]):
-            self.cMap[data.header[1]]().Handle(sck,addr,data)
-        else:
-            self.cMap["400"]().Handle(sck,addr,data)
-    #注册子controller
-    def RigistController(self,key,type):
-        if key and type:
-            self.cMap[key]=type
-        else:
-            raise NameError,"key or type is null"
+        self.cMap = {}
 
-mainController=MainController()
-mainController.RigistController("USER",Login.LoginController)
-mainController.RigistController("404",Notfind.NotfindController)
-#获取全局唯一的mainController
+    # 分发数据包到各个子controller
+    # data包括header和body
+    def Handle(self, sck, addr, data):
+        if not (data.has_key("header") or data.has_key("body")):
+            raise NameError, "data.header or data.body is null"
+        if self.cMap.has_key(data["header"][1]):
+            self.cMap[data.header[1]]().Handle(sck, addr, data)
+        else:
+            self.cMap["400"]().Handle(sck, addr, data)
+
+    # 注册子controller
+    def RigistController(self, key, type):
+        if key and type:
+            self.cMap[key] = type
+        else:
+            raise NameError, "key or type is null"
+#注册子controller
+def regist():
+    mainController.RigistController("USER", Login.LoginController)
+    mainController.RigistController("File", File.FileController)
+    mainController.RigistController("404", Notfind.NotfindController)
+# 获取全局唯一的mainController
 def GetInstance():
     global mainController
     if mainController:
         return mainController
     else:
-        mainController=MainController()
+        mainController = MainController()
         return mainController
+
+mainController = MainController()
+regist()
 
