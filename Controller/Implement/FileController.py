636f3f70190ff26a78d7eeedc55bfed391651c67
VanxeHus <13751378996@163.com>
2018-03-31 16:18:03 +0800

    修改各项管理器模块

--- Controller/Implement/FileController.py
+++ Controller/Implement/FileController.py
@@ -1,9 +1,49 @@
 #!usr/bin/python
 #coding=utf-8
+import struct
+
+from Model import MysqlManager
+from Model import SessionManager
+from utils.log import logEvent
+from utils.encrypt import MD5
+from utils.fileReader import FileReader
+
+
 class FileController:
     def __init__(self):
         pass
+    @logEvent.LogEvent("FileHandle")
     def Handle(self,sck,addr,data):
-        print "this is File controller"
-        print data
-        pass
+        body=data["body"]
+        #获取session
+        sess=SessionManager.GetInstance().SessionStart(None,body["sid"])
+
+        #结果
+        resCode=""
+        resReason=""
+        params=""
+        #无效身份
+        if sess is None:
+            resCode="401\t\n"
+            resReason="Unauthorized\t\n"
+        else:
+            ret=FileReader.ReadFile(body["File"])
+            if ret[:4]=="play ":
+                if ret[5:]=="success":
+                    resCode="200\t\n"
+                else:
+                    resCode="500\t\n"
+                resReason="%s\t\n"%ret
+            else:
+                resCode="200\t\n"
+                resReason="ReadFile success"
+                params="files:%s"%ret
+
+        #发送数据
+        headerLen=resCode.__len__()+resReason.__len__()+params.__len__()
+        headerPack=struct.pack(">I4s",headerLen,"File")
+        sck.send(headerPack)
+        sck.send(resCode + resReason + params)
+        # print "resCode:",resCode
+        # print "resReason:",resReason
+        # print "params:",params
--- Controller/Implement/NotfindController.py
+++ Controller/Implement/NotfindController.py
@@ -1,9 +1,20 @@
 #!usr/bin/python
 #coding=utf-8
+import struct
+from utils.log import logEvent
 class NotfindController:
     def __init__(self):
         pass
+    #404
+    @logEvent.LogEvent("NotfindHandle")
     def Handle(self,sck,addr,data):
-        print "this is Notfind controller"
-        print data
-        pass
+        resCode="404\t\n"
+        resReason="instruction wrong\t\n"
+
+        headerLen=resCode.__len__()+resReason.__len__()
+        headerPack=struct.pack(">I4s",headerLen,"404")
+        sck.send(headerPack)
+        sck.send(resCode + resReason)
+        # print "resCode:",resCode
+        # print "resReason:",resReason
+        # print "params:",params
